0664e264e469b01f2e483ee0421bfb78af3542c2
Aaron <teamshortcut@users.noreply.github.com>
2019-09-21 09:49:23 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install python3.6
+sudo apt-get install python3.7
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3.6 get-pip.py
 sudo pip install requests
-sudo pip install requests_html
 sudo pip install discord
 wget https://raw.githubusercontent.com/teamshortcut/Discord-Maths-Bot/master/maths-bot.py
 sed -i 's/TOKEN[[:space:]]=[[:space:]]\"XXX\"/TOKEN = \"YOURTOKEN\"/g' maths-bot.py
