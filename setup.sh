f424f01171529ba1f84097f2b55053a5cca7785b
Aaron <teamshortcut@users.noreply.github.com>
2019-09-21 09:49:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ sudo python3.6 get-pip.py
 sudo pip install requests
 sudo pip install discord
 wget https://raw.githubusercontent.com/teamshortcut/Discord-Maths-Bot/master/maths-bot.py
+wget https://raw.githubusercontent.com/teamshortcut/Discord-Maths-Bot/master/challenge.txt
 sed -i 's/TOKEN[[:space:]]=[[:space:]]\"XXX\"/TOKEN = \"YOURTOKEN\"/g' maths-bot.py
 wget https://gist.githubusercontent.com/teamshortcut/547b8561ddc1120104c08cc7f4a5ea46/raw/7abca156c50f6b708232d98a8959510ee2dc8e8c/ensurebotrunning.sh
 chmod +x ensurebotrunning.sh
